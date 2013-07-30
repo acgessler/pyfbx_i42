ef96ab36763025d200f4d2da7e1b225601bfa624
Campbell Barton <ideasman42@gmail.com>
2013-07-31 06:58:43 +1000

    data types, convenience module

---
+++ pyfbx/data_types.py
@@ -0,0 +1,35 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
+# Script copyright (C) 2006-2012, assimp team
+# Script copyright (C) 2013 Blender Foundation
+
+INT16 = b'Y'[0]
+BOOL = b'C'[0]
+INT32 = b'I'[0]
+FLOAT32 = b'F'[0]
+FLOAT64 = b'D'[0]
+INT64 = b'L'[0]
+BYTES = b'R'[0]
+STRING = b'S'[0]
+FLOAT32_ARRAY = b'f'[0]
+INT32_ARRAY = b'i'[0]
+FLOAT64_ARRAY = b'd'[0]
+INT64_ARRAY = b'l'[0]
