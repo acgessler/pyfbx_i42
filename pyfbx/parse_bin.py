b3902677ef7e732a6ff4ad50ee2ad6ae39a34db1
Campbell Barton <ideasman42@gmail.com>
2013-07-31 09:28:03 +1000

    rename and some minor changes

--- pyfbx/parse_bin.py
+++ pyfbx/parse_bin.py
@@ -25,9 +25,10 @@ __all__ = (
     "parse",
     )
 
-import struct
 from struct import unpack
+from collections import namedtuple
 import array
+import zlib
 
 # at the end of each nested block, there is a NUL record to indicate
 # that the sub-scope exists (i.e. to distinguish between P: and P : {})
@@ -36,6 +37,9 @@ _BLOCK_SENTINEL_LENGTH = 13
 _BLOCK_SENTINEL_DATA = (b'\0' * _BLOCK_SENTINEL_LENGTH)
 _DEBUG = True
 _IS_LITTLE_ENDIAN = (__import__("sys").byteorder == 'little')
+
+
+FBXElem = namedtuple("FBXNode", ("id", "props", "props_type", "elems"))
 
 
 def read_uint(read):
@@ -62,14 +66,13 @@ def unpack_array(read, array_type, array_stride, array_byteswap):
     if encoding == 0:
         pass
     elif encoding == 1:
-        import zlib
         data = zlib.decompress(data)
 
     assert(length * array_stride == len(data))
 
     data_array = array.array(array_type, data)
-    if array_byteswap and _IS_LITTLE_ENDIAN:
-        data_array.byteswap()
+    #if array_byteswap and _IS_LITTLE_ENDIAN:
+    #    data_array.byteswap()
     return data_array
 
 
@@ -80,8 +83,9 @@ read_data_dict = {
     b'F'[0]: lambda read: unpack(b'<f', read(4))[0],  # 32 bit float
     b'D'[0]: lambda read: unpack(b'<d', read(8))[0],  # 64 bit float
     b'L'[0]: lambda read: unpack(b'<q', read(8))[0],  # 64 bit int
-    b'R'[0]: lambda read: read(read_uint(read)),        # binary data
-    b'S'[0]: lambda read: read(read_uint(read)),        # string data
+    b'R'[0]: lambda read: read(read_uint(read)),      # binary data
+    b'S'[0]: lambda read: read(read_uint(read)),      # string data
+
     b'f'[0]: lambda read: unpack_array(read, 'f', 4, False),  # array (float)
     b'i'[0]: lambda read: unpack_array(read, 'i', 4, True),   # array (int)
     b'd'[0]: lambda read: unpack_array(read, 'd', 8, False),  # array (double)
@@ -89,7 +93,7 @@ read_data_dict = {
     }
 
 
-def read_chunk(read, tell, level):
+def read_elem(read, tell, level):
     # [0] the offset at which this block ends
     # [1] the number of properties in the scope
     # [2] the length of the property list
@@ -100,22 +104,22 @@ def read_chunk(read, tell, level):
     prop_count = read_uint(read)
     prop_length = read_uint(read)
 
-    token_id = read_string_ubyte(read)       # token name of the scope/key
-    token_prop_type = bytearray(prop_count)  # token property types
-    token_prop_data = [None] * prop_count    # token properties (if any)
-    token_children = []                      # token children (if any)
+    elem_id = read_string_ubyte(read)        # elem name of the scope/key
+    elem_props_type = bytearray(prop_count)  # elem property types
+    elem_props_data = [None] * prop_count    # elem properties (if any)
+    elem_subtree = []                        # elem children (if any)
 
     for i in range(prop_count):
         data_type = read(1)[0]
-        token_prop_type[i] = data_type
-        token_prop_data[i] = read_data_dict[data_type](read)
+        elem_props_data[i] = read_data_dict[data_type](read)
+        elem_props_type[i] = data_type
 
     # check if we have nested data
     #~ print("    " * level,
-    #~       "%r: %r ~ %r" % (token_id, token_prop_data, token_prop_type))
+    #~       "%r: %r ~ %r" % (elem_id, elem_props_data, elem_props_type))
     if tell() < end_offset:
         while tell() < (end_offset - _BLOCK_SENTINEL_LENGTH):
-            token_children.append(read_chunk(read, tell, level + 1))
+            elem_subtree.append(read_elem(read, tell, level + 1))
 
         if read(_BLOCK_SENTINEL_LENGTH) != _BLOCK_SENTINEL_DATA:
             raise IOError("failed to read nested block sentinel, "
@@ -124,7 +128,7 @@ def read_chunk(read, tell, level):
     if tell() != end_offset:
         raise IOError("scope length not reached, something is wrong")
 
-    return token_id, token_prop_type, token_prop_data, token_children
+    return FBXElem(elem_id, elem_props_data, elem_props_type, elem_subtree)
 
 
 def parse(fn):
@@ -135,6 +139,7 @@ def parse(fn):
     read = f.read
     tell = f.tell
 
+    # after the text, but we ignore for now: b'  \x00\x1a'
     HEAD_MAGIC = b'Kaydara FBX Binary'
     HEAD_OFFSET = 27
     if read(len(HEAD_MAGIC)) != HEAD_MAGIC:
@@ -142,13 +147,13 @@ def parse(fn):
 
     read(HEAD_OFFSET - tell())
 
-    output = []
+    root_elems = []
 
     while True:
-        child = read_chunk(read, tell, 0)
-        if child is None:
+        elem = read_elem(read, tell, 0)
+        if elem is None:
             break
-        output.append(child)
+        root_elems.append(elem)
 
     print("done in %.4f sec" % (time.time() - t))
-    return output
+    return FBXElem(b'', [], bytearray(0), root_elems)
